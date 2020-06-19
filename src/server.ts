3743e3558000750869f259e6bf24199047359a74
jaca <jaca@gmail.com>
2020-06-19 20:25:05 +0200

    add addProduct function

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import Express from "express";
 import graphqlHTTP from "express-graphql";
+import expressPlayground from "graphql-playground-middleware-express";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
@@ -21,8 +22,15 @@ const main = async () => {
       graphiql: true,
     })
   );
-  app.listen(4000);
-  console.log("Running a GraphQL API server at http://localhost:4000/graphql");
+
+  app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+  app.listen(process.env.PORT || 4000);
+
+  console.log(
+    `Running a GraphQL API server at http://localhost:${
+      process.env.PORT || 4000
+    }/playground`
+  );
 };
 
-main();
+main().catch((err) => console.error(err));
